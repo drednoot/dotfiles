003f9c00ee41e150b29767fe2672a6753fc331b3
ns <ns@localhost.localdomain>
2024-08-31 17:20:09 +0300

    add null_ls rust formatting rustfmt

--- .config/nvim/lua/plugins/null_ls.lua
+++ .config/nvim/lua/plugins/null_ls.lua
@@ -10,6 +10,7 @@ return {
 		null_ls.setup({
 			sources = {
 					null_ls.builtins.formatting.clang_format,
+					null_ls.builtins.formatting.rustfmt,
 			},
 			on_attach = function(client, bufnr)
 					if client.supports_method("textDocument/formatting") then
